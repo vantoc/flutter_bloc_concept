241a9c3c9ec8e0cb96df8cf8086c500193fb0d99
vantoc <mayt3156@gmail.com>
2022-11-24 17:20:48 +0700

    Route with BlocProvider create

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_concept/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_concept/presentation/router/app_router.dart';
 import 'package:flutter_bloc_concept/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_concept/presentation/screens/third_screen.dart';
 
@@ -23,29 +24,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        routes: {
-          '/': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: HomeScreen(
-                  title: 'HomeScreen',
-                  color: Colors.blueAccent,
-                ),
-              ),
-          '/second': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: SecondScreen(
-                  title: 'SecondScreen',
-                  color: Colors.redAccent,
-                ),
-              ),
-          '/third': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: ThirdScreen(
-                  title: 'ThirdScreen',
-                  color: Colors.greenAccent,
-                ),
-              ),
-        },
+        onGenerateRoute: AppRouter.onGenerateRoute,
       ),
     );
   }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,39 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_concept/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_concept/presentation/screens/home_screen.dart';
 import 'package:flutter_bloc_concept/presentation/screens/third_screen.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../screens/second_screen.dart';
 
 class AppRouter {
-  Route onGenerateRoute(RouteSettings routeSettings) {
+  final CounterCubit _counterCubit = CounterCubit();
+
+  static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(
-            builder: (_) => HomeScreen(
-                  title: 'HomeScreen',
-                  color: Colors.blueAccent,
+            builder: (_) => BlocProvider(
+                  create: (context) => CounterCubit(),
+                  child: HomeScreen(
+                    title: 'HomeScreen',
+                    color: Colors.blueAccent,
+                  ),
                 ));
         break;
       case '/second':
         return MaterialPageRoute(
-            builder: (_) => SecondScreen(
-                  title: 'SecondScreen',
-                  color: Colors.redAccent,
+            builder: (_) => BlocProvider(
+                  create: (context) => CounterCubit(),
+                  child: SecondScreen(
+                    title: 'SecondScreen',
+                    color: Colors.redAccent,
+                  ),
                 ));
         break;
       case '/third':
         return MaterialPageRoute(
-            builder: (_) => ThirdScreen(
-                  title: 'ThirdScreen',
-                  color: Colors.redAccent,
+            builder: (_) => BlocProvider(
+                  create: (context) => CounterCubit(),
+                  child: ThirdScreen(
+                    title: 'ThirdScreen',
+                    color: Colors.redAccent,
+                  ),
                 ));
         break;
       default:
         return MaterialPageRoute(
-            builder: (_) => HomeScreen(
-                  title: 'HomeScreen',
-                  color: Colors.blueAccent,
+            builder: (_) => BlocProvider(
+                  create: (context) => CounterCubit(),
+                  child: HomeScreen(
+                    title: 'HomeScreen',
+                    color: Colors.blueAccent,
+                  ),
                 ));
     }
+  }
+
+  void dispose() {
+    _counterCubit.close();
   }
 }
