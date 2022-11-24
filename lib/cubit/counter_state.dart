fc25c6762b0e85501918c6a6551fc5f878579a76
vantoc <mayt3156@gmail.com>
2022-11-24 12:13:17 +0700

    Flutter use BlocProvider

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,16 @@
+part of 'counter_cubit.dart';
+
+// @immutable
+// abstract class CounterState {}
+
+// class CounterInitial extends CounterState {}
+
+class CounterState {
+  int counterValue;
+  //bool wasIncremented;
+
+  CounterState({
+    required this.counterValue,
+    //required this.wasIncremented,
+  });
+}
