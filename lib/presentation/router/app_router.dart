7cb0df8c5dabb4bd00a6d43bb8efde2f0b6fa940
vantoc <mayt3156@gmail.com>
2022-11-24 17:11:38 +0700

    use router without bloc

---
+++ lib/presentation/router/app_router.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_concept/presentation/screens/home_screen.dart';
+import 'package:flutter_bloc_concept/presentation/screens/third_screen.dart';
+
+import '../screens/second_screen.dart';
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) => HomeScreen(
+                  title: 'HomeScreen',
+                  color: Colors.blueAccent,
+                ));
+        break;
+      case '/second':
+        return MaterialPageRoute(
+            builder: (_) => SecondScreen(
+                  title: 'SecondScreen',
+                  color: Colors.redAccent,
+                ));
+        break;
+      case '/third':
+        return MaterialPageRoute(
+            builder: (_) => ThirdScreen(
+                  title: 'ThirdScreen',
+                  color: Colors.redAccent,
+                ));
+        break;
+      default:
+        return MaterialPageRoute(
+            builder: (_) => HomeScreen(
+                  title: 'HomeScreen',
+                  color: Colors.blueAccent,
+                ));
+    }
+  }
+}
